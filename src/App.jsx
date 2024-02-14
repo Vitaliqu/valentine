c6605f91b8c3efde80096ad62cfbe6b537ad6a1a
vitaliip <kelezar4477@gmail.com>
2024-02-14 10:36:28 +0100

    result

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.module.css'
+import FirstPage from "./firstPage.jsx";
+import SecondPage from "./secondPage.jsx";
+
+function App() {
+    return <FirstPage/>;
+}
+
+export default App
